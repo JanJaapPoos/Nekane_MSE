36f729abcd7e779548a3da2cd450d09a9be5aedc
Jan Jaap Poos <janjaap.poos@wur.nl>
2017-06-26 11:56:30 +0200

    fixed migration

--- simple model/simplemodel.R
+++ simple model/simplemodel.R
@@ -15,7 +15,7 @@ source("functions.R")
 
 population_dynamics <- function(pop, startyear, endyear, season, natmortality, catches, recruitment, migration){
   #pop[age,year, season, area]                                                                                                        
-  IreceiveallmigInToArea <- array(0, dim=dim(pop), dimnames= dimnames(pop))
+ MigToArea <- array(0, dim=dim(migration), dimnames= dimnames(migration))
   for (y in (startyear:endyear)){     # need to think this, but maybe is better change last year (y in (endy:(endy +10)))           
     for (ss in (1:length(season))){
       # move time ---------------------                                                             
@@ -41,16 +41,17 @@ population_dynamics <- function(pop, startyear, endyear, season, natmortality, c
       
       #migration
       for (age in (1:dim(pop)[1])){
+        MigToArea[age,1,as.character(ss),,] <- 0
         for (toarea in (dimnames(pop)[4][[1]])){
-          IreceiveallmigInToArea[age,as.character(y),as.character(ss),toarea] <- pop[age,as.character(y),as.character(ss),toarea]
           for (fromarea in (dimnames(pop)[4][[1]])){
-            IreceiveallmigInToArea[age,as.character(y),as.character(ss),toarea] <-  IreceiveallmigInToArea[age,as.character(y),as.character(ss),toarea] + pop[age,as.character(y),as.character(ss),fromarea] * migration[age,1,as.character(ss),fromarea, toarea]
-            browser()
-            pop[age,as.character(y),as.character(ss),toarea]  <-  IreceiveallmigInToArea[age,as.character(y),as.character(ss),toarea]
+            MigToArea[age,1,as.character(ss),toarea, fromarea] <-  MigToArea[age,1,as.character(ss),toarea,fromarea] + ( pop[age,as.character(y),as.character(ss),fromarea] * migration[age,1,as.character(ss),fromarea, toarea])
           }
-          
         }
-         
+        for (toarea in (dimnames(pop)[4][[1]])){
+          for (fromarea in (dimnames(pop)[4][[1]])){
+             pop[age,as.character(y),as.character(ss),toarea]  <-  pop[age,as.character(y),as.character(ss),toarea] +  MigToArea[age,1,as.character(ss),toarea, fromarea]
+          }
+        }
       }
     }
   }
@@ -113,7 +114,7 @@ ages          <- 1:4
 season        <- 1:6
 areas         <- c("a", "b")
 stab.model    <- 10
-NUMRUNS       <- 20
+NUMRUNS       <- 10
 SIMNUMBER     <- 400
 SIGMA         <- 40 #comes from 2
 SPP1DSCSTEPS  <- SPP2DSCSTEPS <- 0
@@ -125,10 +126,10 @@ q             <- 0.0005
 natmortality  <- 0.0001
 recs          <- c(100,0) 
 mig     <- array(0, dim=c(length(ages),1,length(season),length(areas), length(areas)), dimnames=list(cat=ages,year="all",season=as.character(season), from =areas, to=areas)) 
-#mig[,,,"a","a"] <- 1
-#mig[,,,"b","b"] <- 1
-mig[2:4,,2,"a","a"] <- -0.5
-mig[2:4,,2,"a","b"] <- 0.5
+mig[,,,"a","a"] <- -0.2
+mig[,,,"b","b"] <- -0.2
+mig[,,,"a","b"] <- 0.2
+mig[,,,"b","a"] <- 0.2
 aperm( mig,c(1,3,2,4,5))
 
 
@@ -210,7 +211,7 @@ wts
 
 hr <- apply(catches.n.dsvm,1:3,sum)/    (apply(catches.n.dsvm,1:3,sum) +    apply(pop1,1:3,sum) )
 
-pyrs <- 25 
+pyr <- 25 
 
 #what happens in our yield curve for this hr?
 yield_curve(hr=hr[,pyr,], wts, natmortality, R=recs, sequence = 1, verbose=T)
